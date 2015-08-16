75ec00b999586303f542f2d612003eb27831be64
Anton Rufino <anton.vongola@gmail.com>
2015-08-16 21:02:21 +0800

    Solver initialization function prototype completed.

--- js/GOL.js
+++ js/GOL.js
@@ -1,77 +1,46 @@
-function GOL() {
+function Solver() {
 	this.numRows = 0;
 	this.numCols = 0;
-	this.currentGeneration = [];
-	this.nextGeneration = [];
-	this.population = 0;
+	this.maze = [];
+	this.distances = [];
+	this.visited = [];
+	this.path = [];
+	this.numNodes = 0;
 }
 
 GOL.prototype.init = function () {
 	this.numRows = Math.floor((canvas.height - 1) / Life.UI.cellSize);
 	this.numCols = Math.floor((canvas.width - 1) / Life.UI.cellSize);
+	this.numNodes = numRows * numCols;
 	
-	for(var i = 0; i < this.numRows; ++i) {
-		this.currentGeneration[i] = [];
-		this.nextGeneration[i] = [];
-		
-		for(var j = 0; j < this.numCols; ++j) {
-			this.currentGeneration[i][j] = false;
-			this.nextGeneration[i][j] = false;
-		}
-	}
-}
-
-GOL.prototype.getNumberOfNeighbors = function (row, col) {
-	var neighbors = [
-                this.currentGeneration[row][(col - 1 + this.numCols) % this.numCols],
-                this.currentGeneration[(row + 1 + this.numRows) % this.numRows][(col - 1 + this.numCols) % this.numCols],
-                this.currentGeneration[(row + 1 + this.numRows) % this.numRows][col],
-                this.currentGeneration[(row + 1 + this.numRows) % this.numRows][(col + 1 + this.numCols) % this.numCols],
-                this.currentGeneration[row][(col + 1 + this.numCols) % this.numCols],
-                this.currentGeneration[(row - 1 + this.numRows) % this.numRows][(col + 1 + this.numCols) % this.numCols],
-                this.currentGeneration[(row - 1 + this.numRows) % this.numRows][col],
-                this.currentGeneration[(row - 1 + this.numRows) % this.numRows][(col - 1 + this.numCols) % this.numCols]
-            ];
-	var numNeighbors = 0;
-	
-	for (var i = 0; i < neighbors.length; ++i) {
-		if (neighbors[i]) {
-			++numNeighbors;
-		}
+	for (var i = 0; i < numNodes; ++i) {
+	    this.distances[i] = null;
+	    this.visited[i] = false;
+	    this.path[i] = null;
 	}
 	
-	return numNeighbors;
-}
-
-GOL.prototype.getNextGeneration = function () {
-	var i, j;
-	
-	this.nextGeneration = this.currentGeneration;
-	for (i = 0; i < this.numRows; ++i) {
-		for (j = 0; j < this.numCols; ++j) {
-			var count = this.getNumberOfNeighbors(i, j);
-			
-			if (this.currentGeneration[i][j]) {
-				if (count < 2 || count > 3) {
-					this.nextGeneration[i][j] = false;
-				}
-			} else if (count == 3) {
-				this.nextGeneration[i][j] = true;
-			}
-		}
+	for (var i = 0; i < this.numNodes; ++i) {
+	    maze[i] = [];
+	    for (var j = 0; j < this.numNodes; ++j) {
+	        maze[i][j] = false;
+	    }
 	}
-	this.currentGeneration = this.nextGeneration;
-}	
-
-GOL.prototype.getPopulation = function () {
-	var population = 0;
+	
 	for (var i = 0; i < this.numRows; ++i) {
-		for (var j = 0; j < this.numCols; ++j) {
-			if (this.currentGeneration[i][j]) {
-				++population;
-			}
-		}
+	    for (var j = 0; j < this.numCols; ++j) {
+	        currentNode = i * this.numCols + j
+	        if (i != this.numRows - 1 && j != this.numCols -1) {
+	            if (i == this.numRows - 1) {
+	                maze[currentNode][currentNode + 1] = true;
+	            } else if (j = this.numCols - 1) {
+	                maze[currentNode + 1][currentNode] = true;
+	            } else {
+	                maze[currentNode][currentNode + 1] = true;
+	                maze[currentNode + 1][currentNode] = true;
+	            }
+	        }
+	    }
 	}
-	
-	this.population = population;
 }
+
+
